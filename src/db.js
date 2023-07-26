faa695203d0ec7caede46fc141ab2662ce7970ce
Ismael Agustín <50113883+dodonk4@users.noreply.github.com>
2023-07-26 20:59:37 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -19,7 +19,13 @@ let pool;
 if (process.env.DATABASE_URL) {
   // En producción (Render), utiliza la conexión del archivo .env (DATABASE_URL)
   pool = new Pool({
-    connectionString: process.env.DATABASE_URL + "?sslmode=require",
+    // connectionString: process.env.DATABASE_URL + "?sslmode=require",
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+    port: 5432, // Puerto por defecto de PostgreSQL
+  });
   });
 } else {
   // En entorno local, utiliza la conexión interna
