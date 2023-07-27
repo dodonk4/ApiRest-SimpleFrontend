a03bdabd3b7d787763fa07ec6a56f1eab14cbaf8
Ismael Agustín <50113883+dodonk4@users.noreply.github.com>
2023-07-27 12:18:32 -0300

    Create postgreSQLUsersGet.js

---
+++ src/controllers/postgreSQL/postgreSQLUsersGet.js
@@ -0,0 +1,13 @@
+import { pool } from "../../db.js";
+
+const postgreSQLUsersGet = (req, res) => {
+    pool.query('SELECT * FROM users', (err, result) => {
+        if (err) {
+            console.error('Query error:', err)
+        } else {
+            res.send(result.rows)
+        }
+    })
+}
+
+export default postgreSQLUsersGet;
